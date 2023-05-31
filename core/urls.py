f35c812c70f568cfb3771dfd572f986a99fe855c
Fife Oluwabunmi <oluwabunmifife@gmail.com>
2023-05-31 16:05:21 +0100

    Detail template file

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "core"
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
-    
+
     # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
 
