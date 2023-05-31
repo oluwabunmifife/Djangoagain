a6cc4ca053afba9e0c1b22f1f9038e1247690611
Fife Oluwabunmi <oluwabunmifife@gmail.com>
2023-05-31 17:34:25 +0100

    Using Generic views

--- core/views.py
+++ core/views.py
@@ -1,24 +1,30 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "core/index.html", context)
+#For working with generic views
+from django.views import generic
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "core/detail.html", {"question": question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "core/results.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "core/index.html"
+    context_object_name = "latest_question_list"
 
+    def get_queryset(self):
+        #Most recent 5 Questions posted
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "core/detail.html"
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "core/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -33,3 +39,19 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("core:results", args=(question_id,)))
+    
+#MANUALLY CODING THE VIEWS
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return render(request, "core/index.html", context)
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "core/detail.html", {"question": question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "core/results.html", {"question": question})
